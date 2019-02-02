21dfa73eafbefe7641795e93835c0e8358acd18a
Sebastian Paszyński <p15sebek@gmail.com>
2019-02-02 15:05:20 +0100

    lang switch

--- script.js
+++ script.js
@@ -29,8 +29,9 @@
 
     function ClearTextarea() {
         elem.value = "";
+        document.querySelector('.btn6').classList.add("animation");
+        setTimeout(() => document.querySelector('.btn6').classList.remove("animation"), 500);
     }
-
     function countLetter() {
         document.getElementById('count-letter').innerHTML = elem.value.length;
     }
@@ -49,17 +50,81 @@
         document.querySelector(".count-connected-first").style.backgroundColor =  "#FFC930";
         document.querySelector(".count-connected-second").style.backgroundColor =  "#58ACE3";
       }
+
+    function changeTimeBtn(btn){
+        document.getElementById(btn).style.transform = "translateX(-130px)"
+    }
+    function mouseOverBtn(btn){
+        document.getElementById(btn).style.transform = "translateX(0)"
+    }
+    function mouseOutBtn(btn){
+        document.getElementById(btn).style.transform = "translateX(-130px)"
+    }
+
+    /* Change case */
     document.querySelector(".btn1").addEventListener("click", UpperCase);
     document.querySelector(".btn2").addEventListener("click", LowerCase);
     document.querySelector(".btn3").addEventListener("click", SentenceCase);
     document.querySelector(".btn4").addEventListener("click", TitleCase);
     document.querySelector(".btn5").addEventListener("click", CamelCase);
     document.querySelector(".btn6").addEventListener("click", ClearTextarea);
+   
+    /* Counting letter word listener */
     elem.addEventListener("input", countLetter);
     elem.addEventListener("input", countWord);
+    
+    /* Change color in counting button */
     document.querySelector(".count-connected-first").addEventListener("mouseover", mouseOver);
     document.querySelector(".count-connected-first").addEventListener("mouseout", mouseOut);
     document.querySelector(".count-connected-second").addEventListener("mouseover", mouseOver);
     document.querySelector(".count-connected-second").addEventListener("mouseout", mouseOut);
+    
+    /* Hoover button effect*/
+    document.querySelector("#btn1").addEventListener("mouseover", () => mouseOverBtn("btn1"));
+    document.querySelector("#btn1").addEventListener("mouseout", () => mouseOutBtn("btn1"));
+    document.querySelector("#btn2").addEventListener("mouseover", () => mouseOverBtn("btn2"));
+    document.querySelector("#btn2").addEventListener("mouseout", () => mouseOutBtn("btn2"));
+    document.querySelector("#btn3").addEventListener("mouseover", () => mouseOverBtn("btn3"));
+    document.querySelector("#btn3").addEventListener("mouseout", () => mouseOutBtn("btn3"));
+    document.querySelector("#btn4").addEventListener("mouseover", () => mouseOverBtn("btn4"));
+    document.querySelector("#btn4").addEventListener("mouseout", () => mouseOutBtn("btn4"));
+    document.querySelector("#btn5").addEventListener("mouseover", () => mouseOverBtn("btn5"));
+    document.querySelector("#btn5").addEventListener("mouseout", () => mouseOutBtn("btn5"));
+    document.querySelector("#btn6").addEventListener("mouseover", () => mouseOverBtn("btn6"));
+    document.querySelector("#btn6").addEventListener("mouseout", () => mouseOutBtn("btn6"));
+    
+    /* Performing the function after some time */
+    setTimeout(() => changeTimeBtn("btn1"), 500);
+    setTimeout(() => changeTimeBtn("btn2"), 600);
+    setTimeout(() => changeTimeBtn("btn3"), 700);
+    setTimeout(() => changeTimeBtn("btn4"), 800);
+    setTimeout(() => changeTimeBtn("btn5"), 900);
+    setTimeout(() => changeTimeBtn("btn6"), 1000);
+
+    var arrLang = {
+        'pl' : {
+            'heading' : 'Konwertuj dowolny tekst',
+            'countLetter' : 'Liczba znaków: ',
+            'countWord' : 'Liczba słów: ',
+            'placeholder' : 'Wpisz lub wklej twój tekst tutaj...'
+        },
+        'en' : {
+            'heading' : 'Convert any text',
+            'countLetter' : 'Character count: ',
+            'countWord' : 'Word count: ',
+            'placeholder' : 'Type or paste your text here...'
+        }
+    };
+
+    $(function(){
+        $('.translate').click(function(){
+           var lang =$(this).attr('id');
+            
+            $('.lang').each(function(){
+             $(this).text(arrLang[lang][$(this).attr('key')])   
+            });
+            $('.langtextarea').attr('placeholder', arrLang[lang]['placeholder']);
+        });
+    });
 
 })();
